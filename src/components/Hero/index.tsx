ced156ac1bf46351991c3895c1ec89c0e1bcac1e
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-07-13 16:35:47 +0900

    Create a functionality for moving crosswise direction

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Circle } from "react-konva";
 import { useAnimationFrameLoop } from "react-timing-hooks";
 
@@ -17,18 +17,22 @@ const arrowKeys = {
   down: createArrowKeys("S", 0, 1),
   left: createArrowKeys("A", -1, 0),
   right: createArrowKeys("D", 1, 0),
-};
+} as const;
 
 const Hero = () => {
   const [pos, setPos] = useState({ x: 100, y: 100 });
 
-  /// ==== loopRight
-  const loopRight = useAnimationFrameLoop(() => {
-    setPos((prevPos) => ({
-      x: prevPos.x + arrowKeys.right.x,
-      y: prevPos.y + arrowKeys.right.y,
-    }));
-  });
+  const loopCreater = (direction: keyof typeof arrowKeys) => {
+    return useAnimationFrameLoop(() => {
+      setPos((prevPos) => ({
+        x: prevPos.x + arrowKeys[direction].x,
+        y: prevPos.y + arrowKeys[direction].y,
+      }));
+    });
+  };
+
+  /// ==== Move to the right
+  const loopRight = loopCreater("right");
 
   useEffect(() => {
     function handleKeyDown(event: globalThis.KeyboardEvent) {
@@ -50,54 +54,17 @@ const Hero = () => {
     };
   }, [loopRight]);
 
-  // /// ==== loopLeft
-  // const loopLeft = useAnimationFrameLoop(() => {
-  //   setPos((prevPos) => ({
-  //     x: prevPos.x + arrowKeys.left.x,
-  //     y: prevPos.y + arrowKeys.left.y,
-  //   }));
-  // });
-  //
-  // useEffect(() => {
-  //   function handleKeyDown(event: globalThis.KeyboardEvent) {
-  //     if (event.code === arrowKeys.left.keyCode) {
-  //       event.preventDefault();
-  //       event.stopPropagation();
-  //
-  //       loopLeft.start();
-  //     }
-  //   }
-  //   function handleKeyUp(event: globalThis.KeyboardEvent) {
-  //     if (event.code === arrowKeys.left.keyCode) {
-  //       event.preventDefault();
-  //       event.stopPropagation();
-  //
-  //       loopLeft.stop();
-  //     }
-  //   }
-  //   window.addEventListener("keydown", handleKeyDown);
-  //   window.addEventListener("keyup", handleKeyUp);
-  //
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyDown);
-  //     window.removeEventListener("keyup", handleKeyUp);
-  //   };
-  // }, [loopLeft]);
+  /// ==== Move to the left
+  const loopLeft = loopCreater("left");
 
   useEffect(() => {
     function handleKeyDown(event: globalThis.KeyboardEvent) {
       if (event.code === arrowKeys.left.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
         loopLeft.start();
       }
     }
     function handleKeyUp(event: globalThis.KeyboardEvent) {
       if (event.code === arrowKeys.left.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
         loopLeft.stop();
       }
     }
@@ -110,62 +77,18 @@ const Hero = () => {
     };
   }, [loopLeft]);
 
-  /// ==== loopDown
-  const loopDown = useAnimationFrameLoop(() => {
-    setPos((prevPos) => ({
-      x: prevPos.x + arrowKeys.down.x,
-      y: prevPos.y + arrowKeys.down.y,
-    }));
-  });
 
-  useEffect(() => {
-    function handleKeyDown(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.down.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        loopDown.start();
-      }
-    }
-    function handleKeyUp(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.down.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        loopDown.stop();
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [loopDown]);
-
-  /// ==== loopUp
-  const loopUp = useAnimationFrameLoop(() => {
-    setPos((prevPos) => ({
-      x: prevPos.x + arrowKeys.up.x,
-      y: prevPos.y + arrowKeys.up.y,
-    }));
-  });
+  /// ==== Move to the up
+  const loopUp = loopCreater("up");
 
   useEffect(() => {
     function handleKeyDown(event: globalThis.KeyboardEvent) {
       if (event.code === arrowKeys.up.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
         loopUp.start();
       }
     }
     function handleKeyUp(event: globalThis.KeyboardEvent) {
       if (event.code === arrowKeys.up.keyCode) {
-        event.preventDefault();
-        event.stopPropagation();
-
         loopUp.stop();
       }
     }
@@ -177,6 +100,29 @@ const Hero = () => {
       window.removeEventListener("keyup", handleKeyUp);
     };
   }, [loopUp]);
+
+  /// ==== Move to the down
+  const loopDown = loopCreater("down");
+
+  useEffect(() => {
+    function handleKeyDown(event: globalThis.KeyboardEvent) {
+      if (event.code === arrowKeys.down.keyCode) {
+        loopDown.start();
+      }
+    }
+    function handleKeyUp(event: globalThis.KeyboardEvent) {
+      if (event.code === arrowKeys.down.keyCode) {
+        loopDown.stop();
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [loopDown]);
 
   return <Circle x={pos.x} y={pos.y} radius={10} fill="black" />;
 };
