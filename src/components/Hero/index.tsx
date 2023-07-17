454144a7abcb4a732ba6458a59130e1c2a089d74
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-07-17 10:29:41 +0900

    Refactor (FACADE design pattern)

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -27,98 +27,35 @@ const Hero = () => {
     });
   };
 
-  /// ==== Move to the right
-  const loopRight = loopCreater("right");
-
-  useEffect(() => {
-    function handleKeyDown(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.right.keyCode) {
-        loopRight.start();
+  const createCrossMotion = (
+    direction: keyof typeof arrowKeys,
+    fn: (direction: keyof typeof arrowKeys) => ReturnType<typeof useAnimationFrameLoop>
+  ) => { 
+    const loop = fn(direction);
+    useEffect(() => {
+      function handleKeyDown(event: globalThis.KeyboardEvent) {
+        if (event.code === arrowKeys[direction].keyCode) {
+          loop.start();
+        } 
+      } 
+      function handleKeyUp(event: globalThis.KeyboardEvent) {
+        if (event.code === arrowKeys[direction].keyCode) {
+          loop.stop();
+        }
       }
-    }
-    function handleKeyUp(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.right.keyCode) {
-        loopRight.stop();
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [loopRight]);
-
-  /// ==== Move to the left
-  const loopLeft = loopCreater("left");
-
-  useEffect(() => {
-    function handleKeyDown(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.left.keyCode) {
-        loopLeft.start();
-      }
-    }
-    function handleKeyUp(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.left.keyCode) {
-        loopLeft.stop();
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [loopLeft]);
-
-
-  /// ==== Move to the up
-  const loopUp = loopCreater("up");
-
-  useEffect(() => {
-    function handleKeyDown(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.up.keyCode) {
-        loopUp.start();
-      }
-    }
-    function handleKeyUp(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.up.keyCode) {
-        loopUp.stop();
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [loopUp]);
-
-  /// ==== Move to the down
-  const loopDown = loopCreater("down");
-
-  useEffect(() => {
-    function handleKeyDown(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.down.keyCode) {
-        loopDown.start();
-      }
-    }
-    function handleKeyUp(event: globalThis.KeyboardEvent) {
-      if (event.code === arrowKeys.down.keyCode) {
-        loopDown.stop();
-      }
-    }
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [loopDown]);
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("keyup", handleKeyUp);
+ 
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keyup", handleKeyUp);
+      }; 
+    }, [loop]);
+  };
+  createCrossMotion("right",loopCreater)
+  createCrossMotion("left", loopCreater) 
+  createCrossMotion("up", loopCreater) 
+  createCrossMotion("down", loopCreater) 
 
   return <Circle x={pos.x} y={pos.y} radius={10} fill="black" />;
 };
