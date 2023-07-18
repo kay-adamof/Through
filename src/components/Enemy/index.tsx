fd370b4a3e7a07f5746f99908278bcbbae5b9b14
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-07-18 20:08:52 +0900

    Add a seeking functionality with timing-hooks

---
+++ src/components/Enemy/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { Circle } from "react-konva";
+import { useAnimationFrameLoop } from "react-timing-hooks";
+
+export const Enemy: React.FC<
+  Pick<IEnemy, "y" | "x" | "oppositeX" | "oppositeY">
+> = (props) => {
+  const [pos, setPos] = useState({ x: 200, y: 200 });
+
+  const dx = pos.x - props.oppositeX;
+  const dy = pos.y - props.oppositeY;
+  const distance = Math.sqrt(dx * dx - dy * dy);
+
+  const loop = useAnimationFrameLoop(() => {
+    setPos((prevPos) => ({
+      x: prevPos.x - dx * 0.05,
+      y: prevPos.y - dy * 0.05,
+    }));
+  });
+
+  useEffect(() => {
+    if (distance < 30 && distance > 0.01) {
+      loop.start();
+    }
+    return () => {
+      loop.stop();
+    };
+  }, [distance]);
+
+  return <Circle x={pos.x} y={pos.y} stroke={"black"} radius={20} />;
+};
